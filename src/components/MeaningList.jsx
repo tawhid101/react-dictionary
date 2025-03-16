77e249d46e2c2d2db774ec43e7fdbea8cbd1393d
MD. Tawhid Hossain <tawhidsdreem60@gmail.com>
2025-03-16 11:15:51 +0600

    better list

--- src/components/MeaningList.jsx
+++ src/components/MeaningList.jsx
@@ -1,24 +1,23 @@
 const MeaningList = ({ word, meanings = [] }) => {
   return (
-    <li key={Math.random()*1000}>
+    <li key={Math.random() * 1000}>
       <div>
         <p>
           {word && <span>Word:</span>} {word}
         </p>
-        {/* <p>
-          {phonetic && <span>Phonetic:</span>} {phonetic}
-        </p> */}
       </div>
       {meanings.map((meaning) => (
         <>
           <p>
             <span>Parts of Speech:</span> {meaning.partOfSpeech}
           </p>
-          <span>Definitions:</span>
+          <span>Definitions</span>
           <div className="definitions-container">
             <ul>
               {meaning.definitions.map((definition) => (
-                <li key={definition.definition}>{definition.definition}</li>
+                <li key={definition.definition} className="definition-list">
+                  {definition.definition}
+                </li>
               ))}
             </ul>
           </div>
